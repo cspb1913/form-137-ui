d6b5e119d862985b471a777869a273af0efd0923
Jason Calalang <jcalalang@gmail.com>
2025-07-17 10:48:12 +0800

    fix auth0 imports for client SDK

--- components/request-detail.tsx
+++ components/request-detail.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { useUser } from "@auth0/nextjs-auth0/client"
+import { useUser } from "@auth0/nextjs-auth0"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
